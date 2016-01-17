a6fe50cc30df0681a51649b92e25afbcda747147
Your Name <zubairghori93@gmail.com>
2016-01-17 20:32:19 +0000

    gps code

--- tracker.py
+++ tracker.py
@@ -1,1 +1,10 @@
-print('hello wolrd')
+from gps import *
+session = gps()
+session.stream(WATCH_ENABLE|WATCH_NEWSTYLE)
+
+while True:
+
+   report = session.next()
+   if report.keys()[0] == 'epx':
+   print("lat=%f\tlon=%f" % (report['lat'],report['lon']))
+   time.sleep(0.5)
